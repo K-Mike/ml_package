032dd86981219ad55d07fa139030d723c429f279
Michael Karchevskiy <michael.karchevsky@toptal.com>
2018-03-22 12:28:07 +0700

    Added confusable-homoglyphs.

---
+++ text/confusable_homoglyphs/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from ._version import get_versions
+
+__author__ = 'Victor Felder'
+__email__ = 'victorfelder@gmail.com'
+__version__ = get_versions()['version']
+del get_versions
+
+# Project from http://confusable-homoglyphs.readthedocs.io/en/latest/apidocumentation.html
+# Fix some issues, but don't remember where
+# TODO write about erros to github
